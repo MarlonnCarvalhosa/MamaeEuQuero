06a8f7bc444c43bf27aa98a0b26725766a1e47fa
Marlonn Carvalhosa <marlonncarvalhosa@hotmail.com>
2018-05-05 19:56:12 -0300

    Pequenos detalhes

--- app/src/main/java/com/marlonncarvalhosa/mamaeeuquero/fragments/LeiloarFragment.java
+++ app/src/main/java/com/marlonncarvalhosa/mamaeeuquero/fragments/LeiloarFragment.java
@@ -6,7 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.marlonncarvalhosa.mamaeeuquero.CadastroActivity;
+import com.marlonncarvalhosa.mamaeeuquero.MainActivity;
 import com.marlonncarvalhosa.mamaeeuquero.R;
 
 /**
@@ -26,5 +29,6 @@ public class LeiloarFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_leiloar, container, false);
     }
+
 
 }
