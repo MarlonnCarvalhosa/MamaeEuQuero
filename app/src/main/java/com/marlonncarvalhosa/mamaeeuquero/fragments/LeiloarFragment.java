86951c1e2ef9f51ca7ed1d7a481b270bb6a1a376
Marlonn Carvalhosa <marlonncarvalhosa@hotmail.com>
2018-05-07 21:02:49 -0300

    atualizei agora

--- app/src/main/java/com/marlonncarvalhosa/mamaeeuquero/fragments/LeiloarFragment.java
+++ app/src/main/java/com/marlonncarvalhosa/mamaeeuquero/fragments/LeiloarFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.marlonncarvalhosa.mamaeeuquero.CadastroActivity;
@@ -16,19 +19,41 @@ import com.marlonncarvalhosa.mamaeeuquero.R;
  * A simple {@link Fragment} subclass.
  */
 public class LeiloarFragment extends Fragment {
+    EditText edit_produto ,edit_descricao;
+    Button leiloar;
+    Spinner categoria;
+    LayoutInflater inflater1;
 
 
     public LeiloarFragment() {
-        // Required empty public constructor
+        idcampo();
+        metodbutton();
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_leiloar, container, false);
+
     }
 
+    private void idcampo() {
+        final View view1 = inflater1.inflate(R.layout.fragment_leiloar,null);
+        edit_produto = view1.findViewById(R.id.edit_nomeProduto);
+        edit_descricao= view1.findViewById(R.id.editText_descricao);
+        categoria=view1.findViewById(R.id.spinnerclasse);
+        leiloar=view1.findViewById(R.id.button_leiloar);
+
+    }
+    private void metodbutton() {
+        leiloar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+    }
 
 }
