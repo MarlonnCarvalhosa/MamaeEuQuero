8481a8a8cd3f8cdd57982099f4c2a364a5c7b196
Marlonn Carvalhosa <marlonncarvalhosa@hotmail.com>
2018-05-11 21:04:31 -0300

    Timer adicionado não completo

--- app/src/main/java/com/marlonncarvalhosa/mamaeeuquero/fragments/InicioFragment.java
+++ app/src/main/java/com/marlonncarvalhosa/mamaeeuquero/fragments/InicioFragment.java
@@ -2,10 +2,13 @@ package com.marlonncarvalhosa.mamaeeuquero.fragments;
 
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.marlonncarvalhosa.mamaeeuquero.R;
 
@@ -15,9 +18,15 @@ import com.marlonncarvalhosa.mamaeeuquero.R;
  */
 public class InicioFragment extends Fragment {
 
+    private TextView countdownText;
+    private Button countdownButton;
+
+    private CountDownTimer countDownTimer;
+    private long timeLeftinMilliseconds = 600000; //10m
+    private boolean timerRunning;
+
 
     public InicioFragment() {
-        // Required empty public constructor
     }
 
 
@@ -25,7 +34,89 @@ public class InicioFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio, container, false);
+        View view =  inflater.inflate(R.layout.fragment_inicio, container, false);
+        idcampo(view);
+        metodbutton();
+        return  view;
+
     }
 
+    public void idcampo (View view){
+
+        countdownText = view.findViewById(R.id.countdown_text);
+        countdownButton = view.findViewById(R.id.countdown_button);
+
+    }
+
+    private void metodbutton() {
+
+        countdownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startStop();
+
+            }
+        });
+    }
+
+    public void startStop(){
+
+        if (timerRunning) {
+
+            stopTimer();
+
+        } else {
+
+            startTimer();
+        }
+
+    }
+
+    public void startTimer() {
+
+        countDownTimer = new CountDownTimer(timeLeftinMilliseconds, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) { // No video manda colocar 1 onde tá escrito millisUntilFinished, mas nõa funciona
+
+                timeLeftinMilliseconds = 1;
+                updateTimer();
+
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+
+        }.start();
+
+        countdownButton.setText("pause");
+        timerRunning = true;
+
+    }
+
+    public void stopTimer() {
+
+        countDownTimer.cancel();
+        countdownButton.setText("start");
+        timerRunning = false;
+
+    }
+
+    public void updateTimer(){
+
+        int minutes = (int) timeLeftinMilliseconds / 60000;
+        int seconds = (int) timeLeftinMilliseconds % 60000 / 1000;
+
+        String timeLeftText;
+
+        timeLeftText = "" + minutes;
+        timeLeftText += ":";
+        if (seconds < 10) timeLeftText += "0";
+        timeLeftText += seconds;
+
+        countdownText.setText(timeLeftText);
+
+    }
 }
