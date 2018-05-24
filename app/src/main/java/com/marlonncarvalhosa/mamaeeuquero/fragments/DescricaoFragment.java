bab97733190c39cc7f55a5458b11f67192e9eb7c
dell <raphaguedes55@hotmail.com>
2018-05-24 13:50:50 -0300

    descriçao produto

--- app/src/main/java/com/marlonncarvalhosa/mamaeeuquero/fragments/DescricaoFragment.java
+++ app/src/main/java/com/marlonncarvalhosa/mamaeeuquero/fragments/DescricaoFragment.java
@@ -6,16 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.marlonncarvalhosa.mamaeeuquero.R;
 import com.marlonncarvalhosa.mamaeeuquero.model.Produto;
 import com.marlonncarvalhosa.mamaeeuquero.utils.ConstantsUtils;
 
 public class DescricaoFragment extends Fragment{
 private Bundle bundle;
-private  Produto
- produto;
+private  Produto produto;
+private ImageView imageView;
+private TextView nomeProduto,lanceProduto,tempoProduto,detalhesProduto;
+private Button btnLance;
 
     public DescricaoFragment() {
         // Required empty public constructor
@@ -27,20 +33,43 @@ private  Produto
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_descricao, container, false);
+        idCampo(view);
 
         initView(view);
 
+
     return view;
+    }
+
+    private void idCampo(View view) {
+        nomeProduto=view.findViewById(R.id.TextProduto);
+        lanceProduto=view.findViewById(R.id.TextValor);
+        tempoProduto=view.findViewById(R.id.TextTempo);
+        detalhesProduto=view.findViewById(R.id.textDetalhes);
+        imageView=view.findViewById(R.id.imageProduto);
     }
 
     private void initView(View view) {
         bundle = getArguments();
         if(bundle!= null){
             produto = (Produto) bundle.getSerializable(ConstantsUtils.PRODUTO);
+            nomeProduto.setText(""+produto.getNome());
+            lanceProduto.setText(produto.getPreco());
+            tempoProduto.setText(produto.getDataInicial());
+            detalhesProduto.setText(produto.getDescrição());
+            String url = produto.getImageUrl();
+
+            try {
+                Glide.with(getActivity())
+                        .load(url)
+                        .into(imageView);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
 
 
         }
-        Toast.makeText(getActivity(), ""+ produto.getNome() + produto.getCat(), Toast.LENGTH_SHORT).show();
+
 
     }
 
